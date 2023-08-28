d2e8d480d4423c97a66eb7e2d32f5e39ad6df693
vimalagithub <139726179+vimalagithub@users.noreply.github.com>
2023-08-28 20:55:12 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.5.251:9000/',
+  serverUrl: 'http://65.1.100.202:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
